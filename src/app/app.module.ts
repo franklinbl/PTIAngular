bfdcf55fb75f78872bfb04abbdc85e5bf4c5e04f
Franklin Barco <franklinbarco98@gmail.com>
2021-10-31 00:05:33 -0400

    Se agregaron componentes de navbar y footer Se coloco el contenido del navbar Se coloco el contenido del footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
